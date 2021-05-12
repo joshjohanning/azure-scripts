ef35e82ecdc9e31265f861c467191afc5eb308da
Joshua Johanning <jjohanning@10thmagnitude.com>
2021-05-12 14:41:38 -0500

    adding $date to file path

--- AzureResourceDump.ps1
+++ AzureResourceDump.ps1
@@ -5,4 +5,5 @@ foreach ($subscription in $Subscription_hash) {
     Set-AzContext -SubscriptionId $subscription.id
     $resource_objects = $resource_objects + (Get-AzResource | Select-Object @{Name = 'Subscription'; Expression = {$subscription.name}}, ResourceType, Name, Location)
 }
-$resource_objects | Select-Object * | Sort-Object -Property @{Expression = "Subscription"; Descending = $True},  @{Expression = "Location"; Ascending = $True}, @{Expression = "ResourceType"; Ascending = $True} | Export-Csv -Path ./20200924-azure-resource-dump.csv
+$date = get-date -format yyyymmdd
+$resource_objects | Select-Object * | Sort-Object -Property @{Expression = "Subscription"; Descending = $True},  @{Expression = "Location"; Ascending = $True}, @{Expression = "ResourceType"; Ascending = $True} | Export-Csv -Path ./$date-azure-resource-dump.csv
