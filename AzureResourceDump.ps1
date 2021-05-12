da1051711c1bc49ca3dd000e9dee35defe734345
Joshua Johanning <jjohanning@10thmagnitude.com>
2021-05-12 14:42:10 -0500

    fixing date input

--- AzureResourceDump.ps1
+++ AzureResourceDump.ps1
@@ -5,5 +5,5 @@ foreach ($subscription in $Subscription_hash) {
     Set-AzContext -SubscriptionId $subscription.id
     $resource_objects = $resource_objects + (Get-AzResource | Select-Object @{Name = 'Subscription'; Expression = {$subscription.name}}, ResourceType, Name, Location)
 }
-$date = get-date -format yyyymmdd
+$date = get-date -format yyyyMMdd
 $resource_objects | Select-Object * | Sort-Object -Property @{Expression = "Subscription"; Descending = $True},  @{Expression = "Location"; Ascending = $True}, @{Expression = "ResourceType"; Ascending = $True} | Export-Csv -Path ./$date-azure-resource-dump.csv
